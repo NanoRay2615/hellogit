165166b8e6fa1e21eceed3a2faf58ec161f62103
NanoRay2615 <NanoRay2615@users.noreply.github.com>
2017-01-25 11:30:49 -0400

    Added Implementation file

---
+++ MyRectangle.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "MyRectangle.h"
+
+
+	
+	void MyRectangle::setlength(int l)
+	{
+		length = l;
+	}
+	void MyRectangle::setwidth(double w)
+	{
+		width = w;
+	}
+
+	int MyRectangle::getlength()
+	{
+		return length;
+	}
+
+	double MyRectangle::getwidth()
+	{
+		return width;
+	}
+
+	double MyRectangle::CalArea()
+	{
+		double total;
+		total = length * width;
+		return total;
+	}
+
+	MyRectangle::MyRectangle()
+	{
+		length = 0;
+		width = 0;
+
+	}
+
+
